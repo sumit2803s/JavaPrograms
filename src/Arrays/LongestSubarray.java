311653d14ab54fe96a37c0ec51541a76d2431148
ACER <sumit.jaiswal8888@gmail.com>
2025-02-25 20:45:46 +0530

    added Array Programs

---
+++ src/Arrays/LongestSubarray.java
@@ -0,0 +1,22 @@
+//Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+package Arrays;
+
+public class LongestSubarray {
+    public static void main(String[] args) {
+        int[] arr={0,1,1,1,0,1,1,0,1};
+        int right=0,left=0,zerocount=0,max=0;
+        for( right=0;right<arr.length;right++){
+            if(arr[right]==0){
+                zerocount++;
+            }
+            while(zerocount>1){
+                if(arr[left]==0){
+                    zerocount--;
+                }
+                left++;
+            }
+            max=Math.max(max,right-left);
+        }
+        System.out.print(max);
+    }
+}
---
+++ src/Arrays/MaximumVowels.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class MaximumVowels {
+    public static void main(String[] args) {
+        String s = "abciiidef"; int k=3;
+        int count=0,max=0;
+        for(int i=0;i<k;i++)
+        {
+            if(isvowel(s.charAt(i)))
+            {
+                count++;
+            }
+        }
+        max=Math.max(max,count);
+        for(int i=k;i<s.length();i++)
+        {
+            if(isvowel(s.charAt(i)))
+            {
+                count++;
+            }
+            if(isvowel(s.charAt(i-k)))
+            {
+                count--;
+            }
+            max=Math.max(max,count);
+        }
+        System.out.println(max);
+    }
+
+    public static boolean isvowel(char c){
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
