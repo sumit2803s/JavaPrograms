e252ea8aac747e0c7e08adcaa36eacb5a9e8e057
ACER <sumit.jaiswal8888@gmail.com>
2025-03-01 13:47:18 +0530

    added Array Programs

---
+++ src/Arrays/MergeSortedArray.java
@@ -0,0 +1,26 @@
+//You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+package Arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0}; int m = 3; int[] nums2 = {2,5,6};
+        int n = 3;
+        int p1=m-1;
+        int p2=m-1;
+        int pmerge=m+n-1;
+
+        while(p2>=0)
+        {
+            if(p1>=0 && nums1[p1]>nums2[p2]){
+                nums1[pmerge--]=nums1[p1--];
+            }
+            else{
+                nums1[pmerge--]=nums2[p2--];
+            }
+        }
+        for(int i=0;i<nums1.length;i++)
+        {
+            System.out.print(nums1[i]+" ");
+        }
+    }
+}
